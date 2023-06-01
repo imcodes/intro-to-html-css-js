c505ba79820af21745333caed16dd13597bc522d
imcodes <ifeanyi.o.michael20@gmail.com>
2023-06-01 11:24:03 +0100

    added Javascript and Modified the CSS, rearranged file structure

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+var loginDiv = document.querySelector('#login-div')
+var registerDiv = document.querySelector('#register-div')
+
+let toggleLoginRegister = () => {
+    loginDiv.classList.toggle('hidden')
+     registerDiv.classList.toggle('hidden')
+}
